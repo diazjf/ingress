b1bfd8904be0e3070ff570c2c9e925c3a6c7aff4
Fernando Diaz <diazjf@us.ibm.com>
2018-08-21 21:54:30 -0500

    Add dependencies for code-generator
    Adds the required dependencies for code-generator to gopkg.toml.
    Also performed a "dep ensure" after and resolved the hack scripts.

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -30,20 +30,15 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-ge
 #  nginxingress:v1alpha1 \
 #  --output-base "$(dirname ${BASH_SOURCE})/../../.."
 
-${CODEGEN_PKG}/generate-groups.sh "deepcopy" \
-  k8s.io/ingress-nginx/internal k8s.io/ingress-nginx/internal \
-  .:ingress \
-  --output-base "$(dirname ${BASH_SOURCE})/../../.."
-
 ${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
   k8s.io/ingress-nginx/internal/apis k8s.io/ingress-nginx/internal/apis \
   configuration:v1alpha1 \
   --output-base "$(dirname ${BASH_SOURCE})/../../.." \
-  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate/boilerplate.go.txt
+  --go-header-file "${SCRIPT_ROOT}/hack/boilerplate/boilerplate.go.txt"
 
 ${GOPATH}/bin/conversion-gen || go build -o "${GOPATH}/bin/conversion-gen" "${CODEGEN_PKG}/cmd/conversion-gen"
 
 ${GOPATH}/bin/conversion-gen --skip-unsafe=true \
   --input-dirs k8s.io/ingress-nginx/internal/apis/configuration/v1alpha1 \
   --output-file-base=zz_generated.conversion \
-  --go-header-file "hack/boilerplate/boilerplate.go.txt"
+  --go-header-file "${SCRIPT_ROOT}/hack/boilerplate/boilerplate.go.txt"
